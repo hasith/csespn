6da543127e97c2ac9971e3df6993f89feee57ae0
Supun Nakandala <SupunN@99x.lk>
2014-02-28 11:10:30 +0530

    update sql bash script

--- database/csespn.sql
+++ database/csespn.sql
@@ -24,6 +24,7 @@ SET time_zone = "+00:00";
 /*Drop the existing csespn databse and create new database*/
 DROP DATABASE IF EXISTS csespn;
 CREATE DATABASE csespn;
+USE csespn;
 
 --
 -- Table structure for table `users`
