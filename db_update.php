d80b7af25c41d9e5c8b6e2970a2392c19449a17c
Supun Nakandala <SupunN@99x.lk>
2014-02-28 14:28:36 +0530

    update DB class

--- db_update.php
+++ db_update.php
@@ -2,7 +2,5 @@
 
 require_once dirname(__FILE__) . '/classes/DB.class.php';
 $db_file_name = dirname(__FILE__) . "/database_backups/csespn.sql";
-echo "Hello World";
 $db = new DB();
-var_dump($db);
-$db->update_db($file_name);
+echo $db->update_db($file_name);
