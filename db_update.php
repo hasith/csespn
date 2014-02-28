b9c74a9e3b786712222498fd858a0d9215280a6c
Supun Nakandala <SupunN@99x.lk>
2014-02-28 14:14:38 +0530

    update sql bash script and sql

--- db_update.php
+++ db_update.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__) . '/classes/DB.class.php';
 $db_file_name = dirname(__FILE__) . "/database_backups/csespn.sql";
-
+echo "Hello World";
 $db = new DB();
+var_dump($db);
 $db->update_db($file_name);
