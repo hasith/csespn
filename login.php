15d0c765a60246201ee524b179997942f0f43c34
Supun Nakandala <SupunN@99x.lk>
2014-02-28 14:48:01 +0530

    update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ $client = new Google_Client();
 $client->setApplicationName("CSE - Partner Nework");
 $client->setClientId('653040219437-io2als5ek0t0s3emn8ucgn0jjcqj90ii.apps.googleusercontent.com');
 $client->setClientSecret('VHILGukxSpsgthmbc8tgBDAC');
-$client->setRedirectUri('http://dotitude.99xtechnology.com/csespn/login.php');
+$client->setRedirectUri('http://localhost/csespn/login.php');
 $client->setScopes(array('profile'));
 
 $oauth2 = new Google_Auth_OAuth2($client);
