876e3d21086dd4fedde9fc65a07c43cf0f2f2426
Supun Nakandala <SupunN@99x.lk>
2014-02-28 11:59:07 +0530

    update sql bash script

--- github_hook.php
+++ github_hook.php
@@ -14,4 +14,8 @@ if (file_exists($LOCAL_REPO)) {
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
 echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
 
+// Updating database
+echo shell_exec("cd {$LOCAL_REPO} && cd database && bash ./db_update.sh");
+
+
 die("done " . mktime());
