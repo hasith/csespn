ed3f351d71292988ceb466bc56a7e3b796a20107
Hasith Yaggahawita <hasithy@hasithy-mac.local>
2014-03-31 14:37:08 +0530

    adding mising method

--- global.inc.php
+++ global.inc.php
@@ -10,3 +10,12 @@ require_once ROOT_DIR . '/classes/StudentTools.class.php';
 require_once ROOT_DIR . '/classes/Technology.class.php';
 require_once ROOT_DIR . '/classes/Session.class.php';
 require_once ROOT_DIR . '/classes/SessionTools.class.php';
+
+
+function oauth_session_exists() {
+  if((is_array($_SESSION)) && (array_key_exists('oauth', $_SESSION))) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
