b838358f381d23de95f151db04b3c59c808bce8f
Hasith Yaggahawita <hasithy@hasithy-mac.local>
2014-04-03 13:57:27 +0530

    moved the html header to a php include

--- database_backups/seed_basedata.sql
+++ database_backups/seed_basedata.sql
@@ -4,4 +4,12 @@
 
 INSERT INTO `roles` (`id`, `name`) VALUES
 (1, 'Company'),
-(2, 'Student');
+(2, 'Student'),
+(3, 'Admin');
+
+
+INSERT INTO `csespn`.`batches` (`id`, `display_name`, `year`) VALUES
+(1, Batch 10, 2010),
+(2, Batch 11, 2011),
+(3, Batch 12, 2012),
+(4, Batch 13, 2013);
