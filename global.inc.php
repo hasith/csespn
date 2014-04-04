7efff634834f74b94ec1bddb47ed6a3585b91593
Supun Nakandala <supun.nakandala@gmail.com>
2014-04-04 23:26:20 +0530

    removed unused class includes

--- global.inc.php
+++ global.inc.php
@@ -3,8 +3,7 @@
 define('ROOT_DIR', dirname(__FILE__));
 
 require_once ROOT_DIR . '/classes/User.class.php';
-require_once ROOT_DIR . '/classes/Organization.class.php';
-require_once ROOT_DIR . '/classes/Role.class.php';
+require_once ROOT_DIR . '/classes/Company.class.php';
 require_once ROOT_DIR . '/classes/Student.class.php';
 require_once ROOT_DIR . '/classes/StudentTools.class.php';
 require_once ROOT_DIR . '/classes/Technology.class.php';
