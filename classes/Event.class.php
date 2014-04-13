57d5dd1fc1d638ed4380bf97c3ca30b6bd09f8a1
Udith <uditharosha@gmail.com>
2014-04-13 17:04:48 +0530

    Events table added

---
+++ classes/Event.class.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once ROOT_DIR . '/classes/DB.class.php';
+
+class Event {
+
+    public $id;
+    public $title;
+    public $description;
+    public $date;
+    public $time;
+    public $venue;
+    public $url;
+
+    function __construct($data) {
+        $this->id = (isset($data['id'])) ? $data['id'] : "";
+        $this->title = (isset($data['title'])) ? $data['title'] : "";
+        $this->description = (isset($data['description'])) ? $data['description'] : "";
+        $this->date = (isset($data['date'])) ? $data['date'] : "";
+        $this->time = (isset($data['time'])) ? $data['time'] : "";
+        $this->venue = (isset($data['venue'])) ? $data['venue'] : "";
+        $this->url = (isset($data['url'])) ? $data['url'] : "";
+    }
+
+    public static function get($id) {
+        $db = new DB();
+        $result = $db->select('events', "id = $id");
+        return new Event($result[0]);
+    }
+
+}
