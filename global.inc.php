ab2952220bb9cc5585e2cc1f4f92d2246054947c
Chathura <Chathura@Chathura-Lap>
2014-04-17 13:00:48 +0530

    Technology.Tools class included

--- global.inc.php
+++ global.inc.php
@@ -7,6 +7,7 @@ require_once ROOT_DIR . '/classes/Company.class.php';
 require_once ROOT_DIR . '/classes/Student.class.php';
 require_once ROOT_DIR . '/classes/StudentTools.class.php';
 require_once ROOT_DIR . '/classes/Technology.class.php';
+require_once ROOT_DIR . '/classes/TechnologyTools.class.php';
 require_once ROOT_DIR . '/classes/Session.class.php';
 require_once ROOT_DIR . '/classes/SessionTools.class.php';
 require_once ROOT_DIR . '/classes/Batch.class.php';
