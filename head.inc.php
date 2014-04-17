4747a8db84cf7f1eef045aa6fc4b12004ff7a166
Udith <uditharosha@gmail.com>
2014-04-17 08:01:23 +0530

    Event details popup implemented

--- head.inc.php
+++ head.inc.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-  	<!--<link rel="stylesheet" href="css/jquery-ui.min.css">-->
+        <link rel="stylesheet" href="css/jquery-ui.min.css">
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
