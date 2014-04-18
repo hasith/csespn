6433907282a7e5c87f45d6607d3507f2a1d378b1
Chathura <Chathura@Chathura-Lap>
2014-04-18 22:00:44 +0530

    getAll ordered by any field

--- classes/ResearchTools.class.php
+++ classes/ResearchTools.class.php
@@ -1,5 +1,27 @@
 <?php
 
-class ResearchTools{
-    
+require_once ROOT_DIR . '/classes/DB.class.php';
+
+class ResearchTools {
+
+    private $db;
+
+    function __construct() {
+        $this->db = new DB();
+    }
+
+    function getAllResearches($orderBy) {
+        $db = new DB();
+        $results = $db->select2("*", "research", "true", "true", $orderBy);
+        $research = array();
+        foreach ($results as $result) {
+            array_push($research, new Research($result));
+        }
+        return $research;
+    }
+
+    function getResearchByTech($technologyId, $orderBy) {
+        
+    }
+
 }
