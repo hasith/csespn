c76312094b9d878b5771741b036a8c077ac8c2fd
Hasith Yaggahawita <hasithy@hasithy-mac.local>
2014-04-28 00:34:35 +0530

    Home page content and linkes changed. footer added.

--- index.php
+++ index.php
@@ -55,7 +55,13 @@ session_start();
                     	<p>We are Open!</p><p id="signMessage">
                     	CSE Partner Network is a program that helps organizations to grow through an effective affiliation with the department of Computer Science & Engineering at 
                     	<span></span>University of Moratuwa.</span>
-                    	<br/><a class="buttonEffect" href="">Connect with LinkedIn</a><br/><br/><br/>
+                    	<?php if(oauth_session_exists()){
+                                 echo '<a class="buttonEffect" href="./landing.php">Access the Portal</a>';
+                             }else{
+                             	 echo '<a class="buttonEffect" href="./login.php?lType=initiate">Connect with LinkedIn</a>';
+                             }
+                        ?>
+                    	<br/><br/><br/><br/>
                     	We are an open network and you may simply connect us by sign-in. 
                     	<br/><br/>To fully embrase the portal functionality one needs a premium partner account which can be obtained by contacting us.
                     	</p>                      
@@ -63,7 +69,7 @@ session_start();
                     <div id="contactUs" class="clearfix">
                     	<img src="img/contactUs.jpg" />
                         <div id="contactDetails">
-                            <h6>Contact Us</h6>
+                            <h6 class="details_topic">Contact Us</h6>
                             <p>	
                             	Dr. Gamage<br/>Tel: 0112640381-3101
                             </p>
@@ -71,34 +77,45 @@ session_start();
                     </div>                     
                 </div>
             </div>  
-            
-            <div id="benefits" class="hd"><hr><h3>Partner Benifits</h3>  
-            	<div>
-            		<div>image</div>
-            		<div>content</div>
-            	</div>
+            <div id="benefit_bookmark"/>
+            <div  class="hd"><hr><h3>Partner Benifits</h3> </div>
+            <div id="benefits">
+            		<img src="http://lempnet.itcilo.org/images/icon-pax-profile/image_preview" />
+            		<div class="topic details_topic">Access Student Profiles</div>
+            		<div class="text">CSE premium partners will get access to the student registry of all CSE batches. Student profiles are comprises of a brief introduction, competencies, LinkedIn profile, etc. </div>
             </div>                                              
-            <div id="boxesArea">            	
-                <ul>
-                    <li>
-                    	<h2>Collaborate students</h2>
-                        <img src="img/img1.jpg" />
-                    </li>
-                    <li>
-                    	<h2>Be our first preference</h2>
-                        <img src="img/img2.jpg" />
-                    </li>
-                    <li>
-                    	<h2>Partner in research</h2>
-                        <img src="img/img3.jpg" />
-                    </li>
-                    <li>
-                    	<h2>Customize curriculum</h2>
-                        <img src="img/img4.jpg" />
-                    </li>
-                </ul>                
-            </div>
-        </div>        
+            <div id="benefits">
+            		<img src="https://developer.qualcomm.com/sites/default/files/distribute-60px.png" />
+            		<div class="topic details_topic">Assemble a Team</div>
+            		<div class="text">Premium partners will be able to assemble a team of students to assist in their organizational activities such as event organization, CSR work, software development, etc.</div>
+            </div>  
+            <div id="benefits">
+            		<img src="http://medicine.mytau.org/attali/wp-content/uploads/2011/09/Lab-Icon-150x150.png" />
+            		<div class="topic details_topic">Participate in Research </div>
+            		<div class="text">Partner companies will be able to propose research projects (both Final Year and 3rd Year Projects) to be carried out by the students. In addition, partners will be able to follow the research work already happenning in the department to make use of such.</div>
+            </div>  
+            <div id="benefits">
+            		<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTARPqWLr44-bHqa099FZaqMKw51ERCYcwMj7YryKGP7vUqnlcR" />
+            		<div class="topic details_topic">Conduct University Sessions</div>
+            		<div class="text">University needs for external sessions (technical as well as softskills) are listed through this portal. Partner are able to pick the sessions of their interest to facilitate. This is an excellent oppotunity for the organizations of collaborate with the students. In addition partners are able to propose new sessions to be carried out for the students. </div>
+            </div>                          
+       
+            <div id="benefits">
+            		<img src="http://funny-days.com/wp-content/uploads/2014/03/2014-holiday-calendar-good_1395596625.jpg" />
+            		<div class="topic details_topic">Plan for Events</div>
+            		<div class="text">Partners will get the access to department event calandar. This will help the organizations to pre-plan the events to participate and sponsor to recieve the optimum benefits through student interactions. </div>
+            </div>  
+            <div id="benefits">
+            		<img src="https://developer.qualcomm.com/sites/default/files/discuss-60px.png" />
+            		<div class="topic details_topic">Shape-up the Curriculum</div>
+            		<div class="text">Partners are invited for planning meeting of the department activities. This is an oppotunity for the organizations to make a positive impact to the academic curriculum and other event lineup of the department.</div>
+            </div> 
+        <div id="footer">
+        	<hr/>
+        	Developed and maintained by - Computer Science & Engineering Society (CS&ES)
+        </div>
+        
+        
         <script src="js/jquery-1.10.2.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/bjqs-1.3.min.js"></script>
